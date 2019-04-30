a329eb9ee41931ba9f08a856ae865edbc11340b3
JosephNjuguna <josephnjuguna482@gmail.com>
2019-04-30 21:49:15 +0300

    [ft #165712003] add user signup data controller with register function

---
+++ server/controllers/User.js
@@ -0,0 +1,64 @@
+import Usermodel from '../models/Users';
+import Token from '../helpers/Jwt';
+import EncryptData from '../helpers/Encrypt';
+import userDate from '../helpers/Date';
+import Userid from '../helpers/Uid';
+
+const status = 'unverified';
+const isAdmin = false;
+const signedupDate = userDate.date();
+const userId = Userid.uniqueId();
+
+class Authentication {
+
+	static async registerUser(req, res) {
+		try {
+			const {
+				firstname,
+				lastname,
+				address,
+				email,
+				password,
+			} = req.body;
+
+			const hashedPassword = EncryptData.generateHash(password);
+
+			const addUser = new Usermodel({
+				userId,
+				email,
+				firstname,
+				lastname,
+				hashedPassword,
+				address,
+				status,
+				isAdmin,
+				signedupDate,
+			});
+			if (!await addUser.registerUser()) {
+				return res.status(409).json({
+					status: 409,
+					message: 'email already in use',
+				});
+			}
+			const token = Token.generateToken({
+				email,
+				id: addUser.result.user_id,
+				firstname,
+				lastname,
+				address,
+			});
+			return res.status(201).json({
+				status: 201,
+				token,
+				data: addUser.result,
+			});
+		} catch (error) {
+			return res.status(500).json({
+				status: 500,
+				message: 'internal server error',
+			});
+		}
+	}
+};
+
+export default Authentication;
