cf89da90e626355c7d8284d12a0164d116b99bbc
JosephNjuguna <josephnjuguna482@gmail.com>
2019-04-30 21:50:13 +0300

    [ft #165712003] Add array to save user data

---
+++ server/db/users.js
@@ -0,0 +1,28 @@
+const users = [
+	{
+		id: 1,
+		email: 'admin123@gmail.com',
+		firstname: 'main',
+		lastname: 'admin',
+		password: '$2b$10$9/12uo419FFb4NCR3gqvfeB/f6ObMnD2iCu6BXpAdxyHQbDuVD7ta',
+		address: 'database',
+		status: 'unverified',
+		isadmin: true,
+		userid: 'id-i323dwejhyys',
+		signedup_date: '2019/04/18 00:48:08',
+	},
+	{
+		id: 2,
+		email: 'josephnjuguna482@gmail.com',
+		firstname: 'Joseph',
+		lastname: 'Njuguna',
+		password: '$2b$10$9/12uo419FFb4NCR3gqvfeB/f6ObMnD2iCu6BXpAdxyHQbDuVD7ta',
+		address: 'Kenya',
+		status: 'unverified',
+		isadmin: false,
+		userid: 'id-i323dyys',
+		signedup_date: '2019/04/18 00:48:08',
+	}
+];
+
+export default users;
