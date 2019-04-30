853575d1b82f6ff9a1e1b9ff3031c1cc138f4a83
JosephNjuguna <josephnjuguna482@gmail.com>
2019-05-01 02:31:54 +0300

    [ft #165714062] adds middleware to check if user is already authenticated

---
+++ server/middleware/auth.js
@@ -0,0 +1,29 @@
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+class AuthValidator {
+	// check user is authorized
+	static async checkUser(req, res, next) {
+		try {
+			if (!req.headers.authorization || req.headers.authorization === '') {
+				return res.status(400).json({
+					status: 400,
+					message: 'Token  required',
+				});
+			}
+			const token = req.headers.authorization.split(' ')[1];
+			const decoded = jwt.verify(token, process.env.JWT_KEY);
+			req.userData = decoded;
+			next();
+		} catch (e) {
+			res.status(401).json({
+				status: '401',
+				message: 'Auth failed',
+				error: e,
+			});
+		}
+    }
+}
+export default AuthValidator;
