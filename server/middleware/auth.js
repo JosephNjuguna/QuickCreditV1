8ce2bb8eb8e20183d0e4dbefe8b09a5b1d69fdcc
JosephNjuguna <josephnjuguna482@gmail.com>
2019-05-01 13:02:57 +0300

    [ft #165711998] secure api endpoint. admin access only

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -24,6 +24,34 @@ class AuthValidator {
 				error: e,
 			});
 		}
-    }
+	}
+
+	//  check if user is admin
+	static async checkAdmin(req, res, next) {
+		try {
+			if (!req.headers.authorization || req.headers.authorization === '') {
+				return res.status(400).json({
+					status: 400,
+					message: 'Token  required',
+				});
+			}
+			const token = req.headers.authorization.split(' ')[1];
+			const decoded = jwt.verify(token, process.env.JWT_KEY);
+			req.userData = decoded;
+			if (req.userData.email === 'admin123@gmail.com') {
+				next();
+			} else {
+				return res.status(401).json({
+					message: 'Access Denied! You are not allowed to access this route',
+				});
+			}
+		} catch (e) {
+			res.status(401).json({
+				message: 'Auth failed',
+				error: e,
+			});
+		}
+	}
+
 }
 export default AuthValidator;
