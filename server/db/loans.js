9667d0e349920620497980f83e1c814fe6216d3c
JosephNjuguna <josephnjuguna482@gmail.com>
2019-05-03 03:12:33 +0300

    [ft #165711991] update loans dummy data

--- server/db/loans.js
+++ server/db/loans.js
@@ -1,8 +1,7 @@
 const loans = [
     {
-		id: 1,
-		loanId: "id-4nlhju8",
-		user: 'joe@gmail.com',
+		loanId: 2,
+		user: 'josephnjuguna482@gmail.com',
 		requestedOn: '2019/05/1 09:31:08',
 		status: 'pending',
 		repaid: false,
@@ -13,45 +12,8 @@ const loans = [
 		interestRate: 15
 	},
 	{
-		id: 2,
-		user: 'alanm@gmail.com',
-		requestedOn: '2019/05/ 09:31:08',
-		status: 'accepted',
-		repaid: false,
-		tenor: 4,
-        principalAmount: 2000,
-        paymentInstallment: 575,
-        totalAmounttopay: 2300,
-		interestRate: 15
-	},
-	{
-		id: 3,
-		user: 'edwardev@gmail.com',
-		requestedOn: '2019/05/1 09:31:08',
-		status: 'accepted',
-		repaid: false,
-		tenor: 4,
-        principalAmount: 2000,
-        paymentInstallment: 575,
-        totalAmounttopay: 2300,
-		interestRate: 15
-	},
-	{
-		id: 4,
-		user: 'codesnaomi1@gmail.com',
-		requestedOn: '2019/05/1 09:31:08',
-		status: 'accepted',
-		repaid: false,
-		tenor: 4,
-        principalAmount: 2000,
-        paymentInstallment: 575,
-        totalAmounttopay: 2300,
-		interestRate: 15
-	},
-	{	
-		id:5,
-		loanId: 'id-rk9ehmxnx5',
-		user: 'ian001283@gmail.com',
+		loanId: 3,
+		user: 'joe@gmail.com',
 		requestedOn: '2019/05/1 09:31:08',
 		status: 'accepted',
 		repaid: true,
@@ -61,20 +23,17 @@ const loans = [
         totalAmounttopay: 2300,
 		interestRate: 15
 	},
-	{	
-		id:6,
-        loanId: "id-4nlhsokqju8",
-        firstname: "main",
-        lastname: "admin",
-        user: "admin13@gmail.com",
-        requestedOn: "2019/05/1 16:06:46",
-		status: "pending",
-		repaid:true,
+	{
+		loanId: 4,
+		user: 'joe@gmail.com',
+		requestedOn: '2019/05/1 09:31:08',
+		status: 'accepted',
+		repaid: false,
 		tenor: 4,
         principalAmount: 2000,
         paymentInstallment: 575,
         totalAmounttopay: 2300,
 		interestRate: 15
-    }
+	}
 ];
 export default loans;
