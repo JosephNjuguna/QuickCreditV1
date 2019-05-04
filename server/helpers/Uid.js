5b6b918bdd556614fe40312327cd494c43eeb527
JosephNjuguna <josephnjuguna482@gmail.com>
2019-05-04 23:56:25 +0300

    [bg #165808080] update functions generating loan and user id

--- server/helpers/Uid.js
+++ server/helpers/Uid.js
@@ -1,7 +1,12 @@
-class Uid{
-    static uniqueId() {
+import db from '../db/loans';
+class Uid {
+	static uniqueId() {
 		const userId = `id-${Math.random().toString(36).substr(2, 16)}`;
-		return 1;
+		return userId;
+	}
+	static loanId() {
+		const loan_Id = db.length + 1;
+		return loan_Id;
 	}
 }
 export default Uid;
