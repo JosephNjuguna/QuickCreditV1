5c17eb89c509ddc648bc1731cb29ed908e0e0829
JosephNjuguna <josephnjuguna482@gmail.com>
2019-05-05 14:22:52 +0300

    [bg #165808080] eslint fix linting issues

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
-import app from './app';
 import dotenv from 'dotenv';
+import app from './app';
+
 dotenv.config();
 
-const port =  process.env.PORT || 5000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, (err) => {
-  if (err) {
-    console.log(`api not listening on port ${port}`);
-  } else {
-    console.log(`api listening on ${port}`);
-  }
+	if (err) {
+		console.log(`api not listening on port ${port}`);
+	} else {
+		console.log(`api listening on ${port}`);
+	}
 });
