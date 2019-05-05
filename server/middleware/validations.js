9f498e3c47fe3f11b453aeedf853dfa4388177d6
JosephNjuguna <josephnjuguna482@gmail.com>
2019-05-05 14:36:22 +0300

    [bg #165808080] fix validations messages and use response functions

--- server/middleware/validations.js
+++ server/middleware/validations.js
@@ -1,8 +1,6 @@
 import reqResponses from '../helpers/Responses';
 
-let message;
 class Validations {
-	
 	static async validatesignup(req, res, next) {
 		try {
 			const {
@@ -16,49 +14,39 @@ class Validations {
 			let re;
 
 			if (!firstname || firstname === '') {
-				message = 'firstname field empty';
-				return reqResponses.handleError(message, 400, res);
+				return reqResponses.handleError(400, 'firstname field empty', res);
 			}
 			if (!lastname || lastname === '') {
-				message = 'lastname field empty';
-				return reqResponses.handleError(message, 400, res);
+				return reqResponses.handleError(400, 'lastname field empty', res);
 			}
 			if (!address || address === '') {
-				message = 'email field empty';
-				return reqResponses.handleError(message, 400, res);
+				return reqResponses.handleError(400, 'email field empty', res);
 			}
 			if (!email || email === '') {
-				message = 'email field empty';
-				return reqResponses.handleError(message, 400, res);
+				return reqResponses.handleError(400, 'email field empty', res);
 			}
 			if (!password || password === '') {
-				message = 'password field empty';
-				return reqResponses.handleError(message, 400, res);
+				return reqResponses.handleError(400, 'password field empty', res);
 			}
 			if (firstname) {
 				re = /[a-zA-Z]{3,}/;
-				message = 'enter valid firstname';
-				if (!re.test(firstname)) return reqResponses.handleError(message, 400, res);
+				if (!re.test(firstname)) return reqResponses.handleError(400, 'enter valid firstname', res);
 			}
 			if (lastname) {
 				re = /[a-zA-Z]{3,}/;
-				message = 'enter valid lastname';
-				if (!re.test(lastname)) return reqResponses.handleError(message, 400, res);
+				if (!re.test(lastname)) return reqResponses.handleError(400, 'enter valid lastname', res);
 			}
 			if (address) {
 				re = /[a-zA-Z]{3,}_*[0-9_]*[a-zA-Z]*_*/;
-				message = 'address validation failed';
-				if (!re.test(address)) return reqResponses.handleError(message, 400, res);
+				if (!re.test(address)) return reqResponses.handleError(400, 'address validation failed', res);
 			}
 			if (email) {
 				re = /(^[a-zA-Z0-9_.]+@[a-zA-Z0-9-]+\.[a-z]+$)/;
-				message = 'enter valid email e.g user@gmail.com';
-				if (!re.test(email)) return reqResponses.handleError(message, 400, res);
+				if (!re.test(email)) return reqResponses.handleError(400, 'enter valid email e.g user@gmail.com', res);
 			}
 			if (password) {
 				re = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*(\W|_)).{7,}$/;
-				message = 'enter valid password. should be 7 character and more and contain letters and numbers';
-				if (!re.test(password)) return reqResponses.handleError(message, 400, res);
+				if (!re.test(password)) return reqResponses.handleError(400, 'enter valid password. should be 7 character and more and contain letters and numbers', res);
 			}
 			next();
 		} catch (error) {
@@ -75,13 +63,11 @@ class Validations {
 
 			let re;
 			if (email === '' || password === '' || !email || !password) {
-				message = 'Ensure all fields are filled';
-				return reqResponses.handleError(message, 400, res);
+				return reqResponses.handleError(400, 'Ensure all fields are filled', res);
 			}
 			if (email) {
 				re = /(^[a-zA-Z0-9_.]+@[a-zA-Z0-9-]+\.[a-z]+$)/;
-				message = 'enter valid email';
-				if (!re.test(email) || email === '') return reqResponses.handleError(message, 400, res);
+				if (!re.test(email) || email === '') return reqResponses.handleError(400, 'enter valid email', res);
 			}
 			next();
 		} catch (error) {
@@ -129,6 +115,5 @@ class Validations {
 		}
 		next();
 	}
-
 }
 export default Validations;
