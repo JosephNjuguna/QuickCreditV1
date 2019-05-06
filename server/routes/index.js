afd6fc06ea0d0c4516a0c3463130e906bb4420bf
JosephNjuguna <josephnjuguna482@gmail.com>
2019-05-06 21:30:32 +0300

    [ft #165808107] add route to enable admin view all loan payment history

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,11 +1,13 @@
 import users from './users';
 import loans from './loans';
+import payments from './payments';
 
 const appPrefix = '/api/v1';
 
 const routes = (app) => {
 	app.use(appPrefix, users);
 	app.use(appPrefix, loans);
+	app.use(appPrefix, payments);
 };
 
 export default routes;
